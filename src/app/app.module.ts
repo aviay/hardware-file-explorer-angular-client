e972b93f9f27d3ea1c4d0f2d751a7c2475ac180c
Avia Yizhak <aviay@comda.co.il>
2022-02-11 10:14:30 +0200

    Add init components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common'
+import { MatToolbarModule } from '@angular/material/toolbar'
+//import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatIconModule } from '@angular/material/icon'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatMenuModule } from '@angular/material/menu'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { FormsModule } from '@angular/forms';
+import { NewFolderDialogComponent } from './components/dialogs/new-folder-dialog/new-folder-dialog.component';
+import { RenameDialogComponent } from './components/dialogs/rename-dialog/rename-dialog.component'
+//import { FileExplorerComponent } from './file-explorer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFolderDialogComponent,
+    RenameDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    MatToolbarModule,
+//    FlexLayoutModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/file-element.model.ts
@@ -0,0 +1,6 @@
+export class FileElement{
+    public id?: string;
+    public isFolder: boolean = false;
+    public name: string = "";
+    public parent: string = "";
+}
